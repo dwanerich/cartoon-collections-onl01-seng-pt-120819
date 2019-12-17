f7acf34dadd20d36b1db91d5ba57b51b51e6e092
dwane richards <dwanerich@gmail.com>
2019-12-17 04:16:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,11 +8,10 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |call|
     call.capitalize + "!"
   end
-  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+    calls.any do |call|
 end
 
 def find_the_cheese# code an argument here
